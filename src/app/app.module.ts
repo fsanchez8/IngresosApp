e8e86529ee2819a7860d3665edcc475aa043a2d7
fasanche <fasanche@cotrafa.local>
2021-05-26 17:03:54 -0500

    creación el proyecto y estructura de carpetas inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
+import { EstadisticaComponent } from './ingresoEgreso/estadistica/estadistica.component';
+import { DetalleComponent } from './ingresoEgreso/detalle/detalle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    IngresoEgresoComponent,
+    EstadisticaComponent,
+    DetalleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso-egreso',
+  templateUrl: './ingreso-egreso.component.html',
+  styleUrls: ['./ingreso-egreso.component.sass']
+})
+export class IngresoEgresoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
